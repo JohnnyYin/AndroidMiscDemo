acea71e59ca6f3abd17420a016adc65eb293bc7f
Yin Zhong <enjoy930429@gmail.com>
2016-09-04 17:34:57 +0800

    update

--- NinePatchDrawable/src/main/java/me/yinzhong/ninepatchdrawable/MainActivity.java
+++ NinePatchDrawable/src/main/java/me/yinzhong/ninepatchdrawable/MainActivity.java
@@ -10,8 +10,13 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.NinePatchDrawable;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends Activity {
 
@@ -20,22 +25,55 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+            int i = 0;
+
             @Override
             public void onClick(View v) {
-                findViewById(R.id.text).setBackgroundDrawable(decodeNinePatchDrawable());
+                if (i++ % 2 == 0) {
+                    findViewById(R.id.text).setBackgroundDrawable(null);
+                } else {
+                    findViewById(R.id.text).setBackgroundDrawable(decodeNinePatchDrawable());
+                }
+                findViewById(R.id.text).requestLayout();
+                findViewById(R.id.text2).setBackgroundDrawable(decodeNinePatchDrawable());
+                findViewById(R.id.text2).requestLayout();
+                findViewById(R.id.text3).setBackgroundResource(R.drawable.search_background);
             }
         });
     }
 
     private Drawable decodeNinePatchDrawable() {
+        Drawable drawable;
         BitmapFactory.Options op = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getPath() + "/test3.9.png", op);
+
+        InputStream stream = null;
+        Rect padding = new Rect();
+        Bitmap bitmap = null;
+        try {
+            stream = new FileInputStream(Environment.getExternalStorageDirectory().getPath() + "/test3.9.png");
+            bitmap = BitmapFactory.decodeStream(stream, padding, op);
+            bitmap.setDensity(DisplayMetrics.DENSITY_XXHIGH);
+        } catch (Exception e) {
+            /*  do nothing.
+                If the exception happened on open, bm will be null.
+            */
+            Log.e("BitmapFactory", "Unable to decode stream: " + e);
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    // do nothing here
+                }
+            }
+        }
         byte[] chunk = bitmap.getNinePatchChunk();
         boolean result = NinePatch.isNinePatchChunk(chunk);
-        Log.d("TR", "MainActivity.decode:ninepatch = " + result);
         if (result) {
-            return new NinePatchDrawable(bitmap, chunk, new Rect(), null);
+            drawable = new NinePatchDrawable(getResources(), bitmap, chunk, padding, null);
+        } else {
+            drawable = new BitmapDrawable(bitmap);
         }
-        return new BitmapDrawable(bitmap);
+        return drawable;
     }
 }
