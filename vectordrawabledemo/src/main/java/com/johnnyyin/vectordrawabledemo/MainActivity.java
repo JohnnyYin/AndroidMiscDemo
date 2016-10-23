d9a40e75ed654c12c164e12e18f1dbe18bc109ad
Yin Zhong <enjoy930429@gmail.com>
2016-10-23 18:27:48 +0800

    update vector demo

--- vectordrawabledemo/src/main/java/com/johnnyyin/vectordrawabledemo/MainActivity.java
+++ vectordrawabledemo/src/main/java/com/johnnyyin/vectordrawabledemo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.johnnyyin.vectordrawabledemo;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -17,26 +20,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ImageView imageView = (ImageView) findViewById(R.id.animated_vector_drawable);
-//        imageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Drawable drawable = ((ImageView) v).getDrawable();
-//                if (drawable instanceof Animatable) {
-//                    ((Animatable) drawable).start();
-//                }
-//            }
-//        });
-//        ImageView rightImage = (ImageView) findViewById(R.id.right_animated_vector_drawable);
-//        rightImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Drawable drawable = ((ImageView) v).getDrawable();
-//                if (drawable instanceof Animatable) {
-//                    ((Animatable) drawable).start();
-//                }
-//            }
-//        });
+        ImageView imageView = (ImageView) findViewById(R.id.animated_vector_drawable);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = ((ImageView) v).getDrawable();
+                if (drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+            }
+        });
+        ImageView rightImage = (ImageView) findViewById(R.id.right_animated_vector_drawable);
+        rightImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = ((ImageView) v).getDrawable();
+                if (drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+            }
+        });
+
+        final ImageView pausePlayButton = (ImageView) findViewById(R.id.pause_to_play);
+        pausePlayButton.setImageResource(R.drawable.ic_pause_to_play);
+        pausePlayButton.setTag(R.drawable.ic_pause_to_play);
+        pausePlayButton.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                int redId = (int) pausePlayButton.getTag();
+                if (redId == R.drawable.ic_pause_to_play) {
+                    redId = R.drawable.ic_play_to_pause;
+                } else {
+                    redId = R.drawable.ic_pause_to_play;
+                }
+                pausePlayButton.setImageResource(redId);
+                pausePlayButton.setTag(redId);
+                Drawable drawable = ((ImageView) v).getDrawable();
+                if (drawable instanceof Animatable2) {
+                    ((Animatable2) drawable).start();
+                }
+            }
+        });
     }
 
     @Override
